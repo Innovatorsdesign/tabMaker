eb57a773982889f84833f3d5fc7f1dea8ca0722a
Deyan Vatov <d.vacov@gmail.com>
2014-03-19 13:53:35 +0200

    File rename

--- js/jquery.tabMaker.js
+++ js/jquery.tabMaker.js
@@ -3,7 +3,7 @@
 // that are not closed properly. 
 ;(function ($, window, document, undefined) {
 
-//undefined is used here as the undefined global      
+// undefined is used here as the undefined global      
 // variable in ECMAScript 3 and is mutable (i.e. it can      
 // be changed by someone else). undefined isn't really      
 // being passed in so we can ensure that its value is      
